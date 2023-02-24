759f4689668507e843e7b3361bf150ed4bfcf01c
nblackbu <sitdikovaelvinaa96@gmail.com>
2023-02-24 22:59:10 +0300

    Исправления в pom.xml (thymeleaf, resources)

---
+++ Library/src/main/java/com/example/Library/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.example.Library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+    @GetMapping("/")
+    public String ind() {
+        return "books";
+    }
+}
